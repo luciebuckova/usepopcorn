63e6acf6e2669dea6ca99b7165fabe7725767e93
Lucie Bučková <lucie.buckova@gmail.com>
2024-02-17 12:27:06 +0100

    Add custom hooks

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Loader from './Loader';
 import StarRating from './StarRating';
+import { useKey } from './useKey';
 
 const KEY = 'f84fc31d';
 
@@ -13,6 +14,15 @@ export default function MovieDetails({
   const [movie, setMovie] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState('');
+
+  const countRef = useRef(0);
+
+  useEffect(
+    function () {
+      if (userRating) countRef.current = countRef.current + 1;
+    },
+    [userRating]
+  );
 
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(
@@ -41,28 +51,13 @@ export default function MovieDetails({
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(' ').at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
     };
 
     onAddWatched(newWatchedMovie);
     onCloseMovie();
   }
-
-  useEffect(
-    function () {
-      function callback(e) {
-        if (e.code === 'Escape') {
-          onCloseMovie();
-        }
-      }
-
-      document.addEventListener('keydown', callback);
-
-      return function () {
-        document.removeEventListener('keydown', callback);
-      };
-    },
-    [onCloseMovie]
-  );
+  useKey('Escape', onCloseMovie);
 
   useEffect(
     function () {
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,4 +1,12 @@
+import { useEffect, useRef } from 'react';
+
 export default function SearchBar({ query, setQuery }) {
+  const inputElement = useRef(null);
+
+  useEffect(function () {
+    inputElement.current.focus();
+  }, []);
+
   return (
     <input
       className="search"
@@ -6,6 +14,7 @@ export default function SearchBar({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement}
     />
   );
 }
